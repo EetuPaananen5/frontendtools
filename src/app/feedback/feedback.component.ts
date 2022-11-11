d2f95bade88280aa9537b35ad225277b5a3b0c60
EetuPaananen5 <117155998+EetuPaananen5@users.noreply.github.com>
2022-11-11 10:59:54 +0200

    committaa feedback tehtävä

--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Question } from '../question';
 
 @Component({
   selector: 'app-feedback',
@@ -6,6 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feedback.component.css']
 })
 export class FeedbackComponent implements OnInit {
+
+ // questions: String[] = ["Kysymys 1", "Kysymys 2", "Kysymys 3"];
+  questions: Question[] = [new Question("Kysymys 1", "Ei Arvosteltu" ), new Question("Kysymys 2", "Ei Arvosteltu" ), new Question("Kysymys 3", "Ei Arvosteltu")];
+
+  onInputChange(e : any, q: Question): void {
+      //q.answear= e.value; , tulostaa valuen arvon eli numeron
+
+    console.log(e.value);
+    console.log(q.question);
+    console.log(q.answear);
+
+    if(e.value==0){
+      q.answear="ei oo käsitelty";
+      console.log(e.value);
+    }
+    if(e.value==1){
+      q.answear="huono";
+      console.log(e.value);
+    }
+    if(e.value==2){
+      q.answear="iha ok";
+      console.log(e.value);
+    }
+    if(e.value==3){
+      q.answear="hyvä ";
+      console.log(e.value);
+    }
+
+  }
 
   constructor() { }
 
---
+++ src/app/question.ts
@@ -0,0 +1,14 @@
+export class Question {
+    question: string;
+    answear: string;
+
+    constructor(q : string, a: string){
+        this.question = q;
+        this.answear = a;
+    }
+
+
+}
+
+
+
